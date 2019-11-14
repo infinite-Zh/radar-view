36e542b2fa7ad68a384cfbdd5e29108f9b984770
infinite-Zh <zhxuwork@163.com>
2019-11-14 17:46:42 +0800

    clean

--- app/src/main/java/cn/infinite/radarapplication/RadarView.kt
+++ app/src/main/java/cn/infinite/radarapplication/RadarView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import androidx.annotation.ColorRes
 import kotlin.math.*
 import kotlin.random.Random
 
